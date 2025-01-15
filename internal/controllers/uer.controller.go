8f03100b77f8a4a4710867f71de09f4e669e3326
Yang <dethia1toanlyanh@gmail.com>
2025-01-15 16:46:57 +0700

    [add] : implement Register and Login methods in UserController

--- internal/controllers/uer.controller.go
+++ internal/controllers/uer.controller.go
@@ -1,7 +1,21 @@
 package controllers
 
+import (
+	"go-ecommerce-be/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
 type UserController struct{}
 
 func InitUserController() *UserController {
 	return &UserController{}
+}
+
+func (uc *UserController) Register(c *gin.Context) {
+	response.SuccessResponse(c, response.ErrorCodeSuccess, "Register")
+}
+
+func (uc *UserController) Login(c *gin.Context) {
+	response.SuccessResponse(c, response.ErrorCodeSuccess, "Login")
 }
